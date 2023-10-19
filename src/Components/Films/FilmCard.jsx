1b22c3da5120cab2b5896a78d175930321ea83e3
Skittz <skittz.dev@gmail.com>
2023-10-19 08:16:29 -0300

    style: FilmCards boxshadow

--- src/Components/Films/FilmCard.jsx
+++ src/Components/Films/FilmCard.jsx
@@ -20,7 +20,9 @@ const FilmCard = ({
   }
 
   return (
-    <div className={`p-3 cardMD:w-[10.5rem] cardMD:ml-2 cardMD:flex-shrink-0`}>
+    <div
+      className={`p-3 cardMD:pt-5 cardMD:pb-6 cardMD:w-[10.5rem] cardMD:ml-2 cardMD:flex-shrink-0`}
+    >
       <img
         onClick={() => {
           onClick(index);
@@ -28,7 +30,7 @@ const FilmCard = ({
         }}
         className={`rounded-2xl transition ease-in-out delay-600 hover:shadow-[0px_1px_12px_4px_rgba(130,166,237,0.1)] hover:border border-sky-200 border-opacity-10 hover:scale-110 	${
           activeIndex
-            ? 'active animate-scaleCard shadow-[0px_1px_12px_4px_rgba(130,166,237,0.1)] border'
+            ? 'active animate-scaleCard shadow-[0px_0px_8px_4px_rgba(130,166,237,0.1)] border'
             : ' '
         }`}
         src={image}
