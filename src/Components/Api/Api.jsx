ec2604d9939e735bc1701fc7698cbbd5d8eff66c
Skittz <skittz.dev@gmail.com>
2023-10-24 19:44:59 -0300

    feat: new end points like films and review

--- src/Components/Api/Api.jsx
+++ src/Components/Api/Api.jsx
@@ -74,3 +74,133 @@ export async function userPasswordLost_POST(emailUser) {
       throw error;
     });
 }
+
+// Create the user's like ID
+export async function userLikeFilms_POST(token, body) {
+  return await api
+    .post(`/like-films`, body, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error.response.data.error.message;
+    });
+}
+// Movies that the user has already liked
+export async function userLikeFilms_GET(token) {
+  return await api
+    .get('/users/me/?populate=like_films', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      if (response.data.like_films.length !== 0) {
+        return response.data.like_films[0].id;
+      } else {
+        return null;
+      }
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+// Movies based on id Like
+export async function FilmsIdFromLikeId_GET(likeId, token) {
+  return await api
+    .get(`/like-films/${likeId}?populate=filme`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      return response.data.data.attributes.filme.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+// Update id Like Movies list
+export async function userLikeFilms_PUT(token, idLike, body) {
+  return await api
+    .put(`/like-films/${idLike}`, body, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error.response.data.error.message;
+    });
+}
+
+export async function userReview_POST(token, body) {
+  return await api
+    .post(`/reviews`, body, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error.response.data.error.message;
+    });
+}
+
+export async function allReviewsbyFilmId_GET(token, rulePagination, idFilm) {
+  return await api
+    .get(
+      `/reviews?${rulePagination}&filters[filme][id][$eq]=${idFilm}&populate=user.avatar`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+export async function uploadAvatar_POST(bodyFormData, token) {
+  return await api
+    .post(`/upload`, bodyFormData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+export async function showAvatar_GET(token) {
+  return await api
+    .get(`/users/me?populate=avatar`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
