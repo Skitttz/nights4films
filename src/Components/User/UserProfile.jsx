790033596c68eac5e15de605a2be1b024b840b61
Skittz <skittz.dev@gmail.com>
2023-10-28 06:13:53 -0300

    feat: view list films

--- src/Components/User/UserProfile.jsx
+++ src/Components/User/UserProfile.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import { useUserContext, tokenUserLocal } from '../../Hooks/useUser';
-import { uploadAvatar_POST, showAvatar_GET } from '../Api/Api';
+import {
+  uploadAvatar_POST,
+  showAvatar_GET,
+  FilmsIdFromWatchListId_GET,
+  userListFilms_GET,
+} from '../Api/Api';
 import Button from '../Forms/Button';
 import { toast } from 'react-toastify';
 import Head from '../Helper/Head';
+import { Link } from 'react-router-dom';
 
 const UserProfile = () => {
   const { data } = useUserContext();
   const [files, setFiles] = React.useState();
-  const [avatar, setAvatar] = React.useState();
+  const [avatar, setAvatar] = React.useState(null);
+
+  const [watchId, setWatchId] = React.useState();
+  const [list, setList] = React.useState([]);
 
   async function avatarGet() {
     try {
-      showAvatar_GET(tokenUserLocal).then((data) => setAvatar(data.avatar.url));
+      const data = await showAvatar_GET(tokenUserLocal);
+      setAvatar(data.avatar.url);
     } catch (error) {
-      console.error('Erro ao puxar avatar do usuario', error);
+      return null; //Error user not have avatar
     }
   }
-
-  React.useEffect(() => {
-    avatarGet();
-  }, [avatar]);
 
   const uploadImage = async (e) => {
     e.preventDefault();
@@ -31,7 +37,9 @@ const UserProfile = () => {
     formData.append('files', files[0]);
     formData.append('source', 'users-permissions');
     try {
-      uploadAvatar_POST(formData, tokenUserLocal);
+      uploadAvatar_POST(formData, tokenUserLocal).then((response) => {
+        setAvatar(response[0].url);
+      });
       toast.success('Avatar alterado com sucesso!', {
         position: 'top-center',
         autoClose: 5000,
@@ -56,6 +64,32 @@ const UserProfile = () => {
     return `${dia}/${mes}/${ano}`;
   }
 
+  async function getListFilms(token) {
+    try {
+      userListFilms_GET(token).then((data) => setWatchId(data));
+    } catch (err) {
+      console.error('Error ao encontrar ID da lista', err);
+    }
+  }
+
+  async function getIdFilms(watchListId, token) {
+    try {
+      await FilmsIdFromWatchListId_GET(watchListId, token).then((data) =>
+        setList(data),
+      );
+    } catch (err) {
+      console.error('Erro para achar os ID dos filmes', err);
+    }
+  }
+
+  React.useEffect(() => {
+    getListFilms(tokenUserLocal);
+    if (watchId) {
+      getIdFilms(watchId, tokenUserLocal);
+    }
+    avatarGet();
+  }, [watchId]);
+
   return (
     <div className="max-w-5xl mt-16 mx-auto cardMD:p-4 tm:p-2 animate-animeDown">
       <Head title={` » Meu Perfil`} description="Pagina do Perfil" />
@@ -70,7 +104,7 @@ const UserProfile = () => {
         >
           <div className="bg-slate-950 py-4 px-8 rounded-md flex flex-col gap-y-2 items-start">
             <p className="text-2xl font-bold text-orange-200 px-2 py-1 mb-2 border-b border-b-slate-700 rounded-md">
-              Seus Dados
+              Meus Dados
             </p>
             <p className="font-semibold">
               {`Email:`} <span className="font-light">{` ${data.email}`}</span>{' '}
@@ -112,9 +146,28 @@ const UserProfile = () => {
           </div>
         </form>
         <div className="mt-12">
-          <p className="text-2xl cardMD:text-lg font-mono border-b border-b-slate-800 mb-4">
+          <p className="text-2xl cardMD:text-2xl font-mono border-b border-b-slate-800 mb-4">
             {`Minha Lista`}
           </p>
+          <div className="flex flex-wrap gap-x-8 list-none">
+            {list.map((film) => (
+              <div
+                className="flex-grow-1 basis-[160px] sm:mx-auto"
+                key={film.id}
+              >
+                <Link to={`/filmes/${film.attributes.slug}`}>
+                  <img
+                    className="w-[160px] rounded-md box-shadow: 4.0px 8.0px 8.0px rgba(0,0,0,0.38) hover:scale-110 transition-all"
+                    src={film.attributes.card.data.attributes.url}
+                    alt=""
+                  />
+                  <p className="mt-2 font-gabarito font-semibold tm:text-xs text-center">
+                    {film.attributes.title}
+                  </p>
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
