1c8b19a86cb5e7d8771ad6d5aaac37150f2cd503
Skittz <skittz.dev@gmail.com>
2023-10-28 06:08:21 -0300

    feat: Rate component

---
+++ src/Components/User/Rate/RateButton.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Rate, ConfigProvider } from 'antd';
+import { FilmsIdFromRateId_GET } from '../../Api/Api';
+import { userAlreadyRatedFilm } from './UserRate';
+
+const WatchButton = ({
+  tokenUserLocal,
+  films,
+  login,
+  userRateUpdate,
+  userRateRemove,
+  userRateCreateId,
+  data,
+  sizeIcons,
+}) => {
+  const [rate, setRate] = React.useState(0);
+  const [rateId, setRateId] = React.useState('');
+  const [clearRate, setClearRate] = React.useState(false);
+  const [existRate, setExistRate] = React.useState(false);
+
+  React.useEffect(() => {
+    const initAlreadyRated = async () => {
+      const resolve = await userAlreadyRatedFilm(
+        films,
+        login,
+        tokenUserLocal,
+        setRateId,
+        setRate,
+      );
+      setExistRate(resolve);
+    };
+    initAlreadyRated();
+  }, [setRate]);
+
+  const handleRateClick = async (newRate) => {
+    if (existRate === null) {
+      userRateCreateId(tokenUserLocal, films.data[0].id, newRate, data.id);
+    } else {
+      userRateUpdate(tokenUserLocal, rateId, films.data[0].id, newRate);
+    }
+  };
+  return (
+    <>
+      <div
+        onClick={() => {
+          setClearRate(false);
+          setRate(0);
+          handleRateClick(0);
+        }}
+        className={` cursor-pointer -top-0 -right-8 content-exit animate-animeLeft ${
+          clearRate ? 'absolute' : 'hidden'
+        }`}
+      ></div>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorFillContent: 'rgba(255, 255,255, 0.4)',
+          },
+        }}
+      >
+        <Rate
+          allowClear={true}
+          allowHalf
+          value={rate}
+          onChange={(newRate) => {
+            setRate(newRate);
+            handleRateClick(newRate);
+            setClearRate(newRate > 0);
+          }}
+          defaultValue={rate}
+          style={{
+            color: 'rgba(127,76,178)',
+            fontSize: `${sizeIcons}px`,
+          }}
+        />
+      </ConfigProvider>
+      {rate === 0 ? (
+        <p className="text-slate-400">Avaliar</p>
+      ) : (
+        <p className="text-slate-400">Avaliado</p>
+      )}
+    </>
+  );
+};
+
+export default WatchButton;
