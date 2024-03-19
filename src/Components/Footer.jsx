11994cbfbfde0ca569e639b874e8ec57d8d98fdc
SKitttz <skittz.dev@gmail.com>
2024-03-19 11:01:55 -0300

    refact: footer change color when change pathname

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Footer = () => {
+  let { pathname } = useLocation();
   return (
-    <div className="grid justify-center bg-gray-900 py-3 border-t border-t-gray-400  border-opacity-5 cardMD:h-auto z-1">
+    <div
+      className={`grid justify-center items-center ${
+        pathname === '/' ? 'bg-gray-900' : 'bg-slate-950'
+      } py-3 border-t border-t-gray-400 border-opacity-5 cardMD:h-auto z-1`}
+    >
       <p className="font-gabarito text-slate-500">
         © 2023 - 2023 │ Desenvolvido por
         <a
