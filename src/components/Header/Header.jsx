7831dabedd8624f5f531a76fa0ad196b4720ccf4
SKitttz <skittz.dev@gmail.com>
2025-09-28 13:27:50 -0300

    styles(header): fix animation close

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,7 @@ import useDebounce from '../../hooks/useDebounce';
 import { useUserContext } from '../../hooks/useUser';
 import Input from '../Forms/Input';
 import CapitalizeLetter from '../Helper/CapitalizeLetter';
+import styles from './Header.module.css';
 import HeaderSkeleton from './HeaderLoading';
 
 const Header = ({ onSearchValueChange }) => {
@@ -19,6 +20,7 @@ const Header = ({ onSearchValueChange }) => {
   const debouncedChange = useDebounce(onSearchValueChange, 300);
   const { data, userLogout, loading } = useUserContext();
   const [openMenuMB, setOpenMenuMB] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
   const refNav = useRef(null);
   const navigate = useNavigate();
 
@@ -26,6 +28,21 @@ const Header = ({ onSearchValueChange }) => {
   const isDifPathLoginUser = pathname !== '/login';
   const isLoadingHeaderDesktop = loading && isDesktop && isDifPathLoginUser;
 
+  const handleOutsideClick = (event) => {
+    if (
+      refNav.current &&
+      !refNav.current.contains(event.target) &&
+      openMenuMB
+    ) {
+      handleCloseMenu();
+    }
+  };
+
+  if (openMenuMB) {
+    window.addEventListener('click', handleOutsideClick, true);
+  } else {
+    window.removeEventListener('click', handleOutsideClick, true);
+  }
   if (isLoadingHeaderDesktop) {
     return <HeaderSkeleton />;
   }
@@ -39,26 +56,36 @@ const Header = ({ onSearchValueChange }) => {
     setOpenModal(true);
   };
 
-  const handleOutsideClick = (event) => {
-    if (
-      refNav.current &&
-      !refNav.current.contains(event.target) &&
-      openMenuMB
-    ) {
+  const handleCloseMenu = () => {
+    setIsClosing(true); // ativa a animação de fechar
+    setTimeout(() => {
+      setIsClosing(false);
       setOpenMenuMB(false);
-    }
+    }, 250); // Tempo para a animação completar
   };
 
-  if (openMenuMB) {
-    window.addEventListener('click', handleOutsideClick, true);
-  } else {
-    window.removeEventListener('click', handleOutsideClick, true);
-  }
+  const handleToggleMenu = () => {
+    if (openMenuMB && !isClosing) {
+      handleCloseMenu();
+    } else if (!openMenuMB && !isClosing) {
+      setOpenMenuMB(true);
+    }
+  };
 
   const handleSearchChange = (event) => {
     event.preventDefault();
     setSearchValue(event.target.value);
     debouncedChange(event.target.value);
+  };
+
+  const getAnimationClass = () => {
+    if (isClosing) {
+      return styles['mobile-animate-animeUp'];
+    }
+    if (openMenuMB) {
+      return styles['mobile-animate-animeDown'];
+    }
+    return '';
   };
 
   return (
@@ -90,20 +117,23 @@ const Header = ({ onSearchValueChange }) => {
       <header className="fixed z-30 w-full top-0 bg-gray-950 shadow-md">
         <nav
           ref={refNav}
-          className={`h-16 max-w-7xl mx-auto flex flex-wrap items-center justify-between lg:justify-between lg:max-w-5xl tm:max-w-5xl tm:flex-nowrap tm:gap-4 ${
-            openMenuMB
-              ? 'flex-col h-[auto] gap-y-10 mt-4 border-b border-b-purple-900 border-opacity-60 cardMD:animate-animeDown'
-              : 'cardMD:flex-row cardMD:animate-animeTop'
+          className={`h-16 max-w-7xl mx-auto flex flex-wrap items-center justify-between lg:justify-between lg:max-w-5xl tm:max-w-5xl tm:flex-nowrap tm:gap-4 overflow-hidden transition-[max-height] duration-600 ease-in-out ${
+            openMenuMB || isClosing
+              ? `flex-col h-[auto] gap-y-10 mt-4 border-b border-b-purple-900 border-opacity-60 ${getAnimationClass()}`
+              : 'cardMD:flex-row'
           }`}
         >
           <div>
             <Link
               className={`flex items-center select-none  ${
-                openMenuMB ? 'ml-0' : 'ml-7'
+                openMenuMB || isClosing ? 'ml-0' : 'ml-7'
               }`}
               to={'/'}
               onClick={() => {
                 setSearchValue('');
+                if (openMenuMB) {
+                  handleCloseMenu();
+                }
               }}
             >
               <p className="font-gabarito font-thin text-2xl text-gray-200 ">
@@ -116,14 +146,18 @@ const Header = ({ onSearchValueChange }) => {
             </Link>
           </div>
           <FiMenu
-            onClick={() => setOpenMenuMB(!openMenuMB)}
-            className={` hidden cardMD:order-1 cardMD:block h-6 w-6 cursor-pointer text-[rgba(107,80,178,.9)] hover:text-[rgba(107,66,178,1)] cardMD:mr-3 ${
-              openMenuMB ? 'cardMD:mb-4 ml-auto' : ''
+            onClick={handleToggleMenu}
+            className={`hidden cardMD:order-1 cardMD:block h-6 w-6 cursor-pointer text-[rgba(107,80,178,.9)] hover:text-[rgba(107,66,178,1)] cardMD:mr-3 transition-transform duration-200 ${
+              openMenuMB || isClosing
+                ? 'cardMD:mb-4 ml-auto rotate-90'
+                : 'rotate-0'
             }`}
           />
           <div
             className={`flex items-center ${
-              openMenuMB ? 'cardMD:block mt-3 mb-3' : 'cardMD:hidden'
+              openMenuMB || isClosing
+                ? 'cardMD:block mt-3 mb-3'
+                : 'cardMD:hidden'
             }`}
           >
             {onSearchValueChange ? (
@@ -134,8 +168,8 @@ const Header = ({ onSearchValueChange }) => {
                 backgroundPosition={'3% 45%'}
                 customStyleInput={'indent-7 tm:indent-6 '}
                 placeholder={'Digite o nome do filme...'}
-                value={searchValue} // Input value is controlled by the state
-                onChange={handleSearchChange} // Update the state when the input changes
+                value={searchValue}
+                onChange={handleSearchChange}
               />
             ) : (
               ''
@@ -144,7 +178,7 @@ const Header = ({ onSearchValueChange }) => {
           {data ? (
             <div
               className={`${
-                openMenuMB ? 'cardMD:block' : 'cardMD:hidden'
+                openMenuMB || isClosing ? 'cardMD:block' : 'cardMD:hidden'
               }  flex gap-x-2 xm:pr-4`}
             >
               <div
@@ -161,7 +195,15 @@ const Header = ({ onSearchValueChange }) => {
                   </p>
                 </div>
               </div>
-              <Link to={'/perfil'} className="mr-1 tm:mr-0">
+              <Link
+                to={'/perfil'}
+                className="mr-1 tm:mr-0"
+                onClick={() => {
+                  if (openMenuMB) {
+                    handleCloseMenu();
+                  }
+                }}
+              >
                 <div
                   className={`text-purple-100 font-roboto font-light p-2 bg-purple-800 hover:bg-opacity-100 ${
                     pathname === '/perfil' ? 'bg-opacity-100' : 'bg-opacity-60'
@@ -175,12 +217,14 @@ const Header = ({ onSearchValueChange }) => {
               </Link>
               <div
                 className={`rounded-sm py-2 ${
-                  openMenuMB ? 'cardMD:block mt-4' : 'cardMD:hidden'
+                  openMenuMB || isClosing
+                    ? 'cardMD:block mt-4'
+                    : 'cardMD:hidden'
                 } `}
               >
                 <button
                   type="button"
-                  className="cursor-pointer"
+                  className="cursor-pointer hover:scale-110 transition-transform duration-200"
                   onClick={handleOpenModal}
                 >
                   <img
@@ -193,8 +237,15 @@ const Header = ({ onSearchValueChange }) => {
             </div>
           ) : (
             <Link
-              className={` ${openMenuMB ? 'cardMD:block ' : 'cardMD:hidden'}`}
+              className={` ${
+                openMenuMB || isClosing ? 'cardMD:block ' : 'cardMD:hidden'
+              }`}
               to={'/login'}
+              onClick={() => {
+                if (openMenuMB) {
+                  handleCloseMenu();
+                }
+              }}
             >
               <div className=" text-slate-300 hover:text-slate-100 p-2 bg-[rgba(107,46,178)] hover:bg-[rgba(107,20,178)]  rounded-md transition-colors duration-300 font-gabarito mr-4 ">
                 <p>Login / Registrar</p>
