ff32bee03a9d71c1a9a397533eda96991ecbd25f
skitttz <skittz.dev@gmail.com>
2026-02-21 21:59:26 -0300

    styles: improvement footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,25 +2,23 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 const Footer = () => {
-  let { pathname } = useLocation();
+  const { pathname } = useLocation();
   return (
-    <div
-      className={`grid justify-center items-center ${pathname === '/' ? 'bg-gray-900' : 'bg-slate-950'
-        } py-3 border-t border-t-gray-400 border-opacity-5 cardMD:h-auto z-1`}
-    >
-      <p className="font-gabarito text-slate-500">
-        © 2023 - 2023 │ Desenvolvido por
-        <a
-          className="text-slate-100"
-          href="https://github.com/Skitttz"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          {' '}
-          skittz
-        </a>
-      </p>
-    </div>
+    <footer className="w-full bg-gray-900 border-t border-t-gray-400 border-opacity-5 py-4">
+      <div className="max-w-7xl lg:max-w-5xl mx-auto px-4 flex items-center justify-center">
+        <p className="font-gabarito text-slate-500 text-sm sm:text-base">
+          © 2023 - 2026 │ Desenvolvido por
+          <a
+            className="text-slate-100 ml-1"
+            href="https://github.com/Skitttz"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            skittz
+          </a>
+        </p>
+      </div>
+    </footer>
   );
 };
 
